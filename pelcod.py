335a95d2740963ef50b0e40652317087b0445131
sintarahmana12-spec <sintarahmana12@gmail.com>
2026-09-20 08:53:32 +0700

    sinta pelcod

---
+++ pelcod.py
@@ -0,0 +1,15 @@
+waktu = 10.05
+
+if waktu == 10.00:
+    print("tepat waktu")
+elif waktu < 10.00:
+    print("datang lebih awal")
+else:
+    print("telat")
+
+ketentuan = 10.05
+
+if ketentuan < 10.01:
+    print("boleh gabung ke barisan")
+else:
+    print("tutup pintu dari luar")
