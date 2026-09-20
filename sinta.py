d5364dcc7906c64c73e595a209b1a3f7afced70b
sintarahmana12-spec <sintarahmana12@gmail.com>
2026-09-20 10:12:01 +0700

    sinta

---
+++ sinta.py
@@ -0,0 +1,22 @@
+nama_peminjam = "sinta"
+no_anggota = 1
+jumlah_buku = 10
+status = True
+durasi = 7
+
+print(nama_peminjam)
+
+jumlah_tiket = "4"
+harga_tiket = 25000
+
+total_sementara = jumlah_tiket*2
+print("total_sementara:", total_sementara, "harga tiket:", harga_tiket)
+
+jumlah_tiket = "4"
+harga_tiket = 25000
+total_sementara = jumlah_tiket*2
+print("total_sementara:", total_sementara)
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir = jumlah_tiket*harga_tiket
+print("total_akhir:", total_akhir)
